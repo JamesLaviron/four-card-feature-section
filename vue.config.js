fdf344429e96fa705bc7aa0fd1b7948787116634
JamesLaviron <victor@gerfaud.fr>
2024-09-02 13:06:44 +0200

    Clean project base

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/four-card-feature-section/" : "/",
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/_reset.scss";
+          @import "@/assets/styles/base.scss";
+          @import "@/assets/styles/typo.scss";
+          @import "@/assets/styles/_variables.scss";
+          @import "@/assets/styles/text-presets.scss";
+        `
+      }
+    }
+  }
+})
